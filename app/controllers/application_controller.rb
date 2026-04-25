53b96c9fdf1fbb1d6428fb97536d063107dbec41
Ismael Araujo <araujoismael0982@gmail.com>
2026-04-25 13:35:00 -0300

    feat: add before_action(method athorize_request) in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  private
+   
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+    decoded = JwtService.decode(token)
+
+    if decoded
+      @current_user = User.find_by(id: decoded["user_id"])
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
 end
